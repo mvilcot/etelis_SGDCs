37da1333b2569daf30014ed4bfe51d4aae6b7a9c
Maurine Vilcot <maurine.vilcot@gmail.com>
2023-04-14 13:11:47 +0200

    add tess3

--- scripts/1_genetic_diversity/17_structure.R
+++ scripts/1_genetic_diversity/17_structure.R
@@ -8,14 +8,14 @@
 
 # parameters
 filters = "missind1_callrate0.70_maf0.05"
-level = "station"
-sites = "Hawaii"
+level = "site"
+sites = "noCocos"
 
 # read genlight
 genlight <- 
   read.genlight(filters, level,
                 site2drop = NULL,
-                site2keep = "Hawaii",
+                site2keep = NULL,
                 station2drop = NULL,
                 station2keep = NULL)
 
@@ -59,10 +59,56 @@ dev.off()
 
 
 
-## ---- tess3r ----
+## ---- tess3 ----
 library(tess3r)
 
+genotype <- 
+  read.table(paste0("intermediate/1_genetic_diversity/GenoLEA_Etelis_coruscans_ordered_", sites, ".lfmm"))
+genotype[genotype == 9] <- NA
 
+coord <- 
+  data_samples %>% 
+  dplyr::filter(id %in% genlight@ind.names) %>% 
+  dplyr::select(Longitude_approx, Latitude_approx) %>% 
+  as.matrix
+
+# run tess3
+obj.tess3 <- tess3(X = genotype, 
+                   coord = coord, K = 1:3,
+                   method = "projected.ls",
+                   ploidy = 2)
+
+# cross-validation
+plot(obj.tess3, pch = 19, col = "blue",
+     xlab = "Number of ancestral populations",
+     ylab = "Cross-validation score")
+
+# retrieve tess3 Q matrix 
+q.matrix <- qmatrix(obj.tess3, K = 2)
+
+# STRUCTURE-like barplot for the Q-matrix 
+my.colors <- c("tomato", "orange", "lightblue", "wheat","olivedrab")
+my.palette <- CreatePalette(my.colors, 9)
+barplot(q.matrix, border = NA, space = 0, 
+        main = "Ancestry matrix", 
+        xlab = "Individuals", ylab = "Ancestry proportions", 
+        col.palette = my.palette) -> bp
+axis(1, at = 1:nrow(q.matrix), labels = bp$order, las = 3, cex.axis = .4) 
+
+# # map plot
+# library(rworldmap)
+# map.polygon <- getMap(resolution = "low")
+# 
+# pl <- ggtess3Q(q.matrix, coord, map.polygon = map.polygon)
+# pl +
+#   geom_path(data = map.polygon, aes(x = long, y = lat, group = group)) +
+#   xlim(-180, 180) + 
+#   ylim(-60, 60) + 
+#   coord_equal() + 
+#   geom_point(data = as.data.frame(coord), aes(x = Longitude_approx, y = Latitude_approx), size = 0.2) + 
+#   xlab("Longitute") +
+#   ylab("Latitude") + 
+#   theme_bw()
 
 ## ---- STRUCTURE ----
 library(strataG)
