d097d5ff68c690545a7f7b7c5334edd4f3307fd5
Maurine Vilcot <maurine.vilcot@gmail.com>
2023-05-10 17:43:47 +0200

    update alpha diversity table

---
+++ scripts/3_distance_metrics/35_wrangle_alpha_diversity.R
@@ -0,0 +1,28 @@
+
+## ---- parameters ----
+level = "site"
+
+comm_delin = "taxonomic_scale_Fishbase"
+list_communities <- readRDS(paste0("intermediate/2_species_diversity/List_community_", comm_delin, ".RDS"))
+
+# parameters
+metricSD = "richness_site"
+metricGD = "Hs"
+
+
+## ---- load ----
+gd_alpha <- read_csv(paste0("results/1_genetic_diversity/gd_table_", level, ".csv"))
+sd_alpha <- read_csv(paste0("results/2_species_diversity/sd_table_", level, "_", comm_delin, ".csv"))
+
+sd_alpha <-
+  sd_alpha %>% 
+  tidyr::pivot_wider(names_from = community, 
+                     values_from = richness_site)
+
+alpha_merge <-
+  full_join(gd_alpha, sd_alpha, by = level)
+
+
+## ---- save ----
+alpha_merge %>% 
+  write_csv(paste0("results/3_distance_metrics/alpha_diversity_", comm_delin, ".csv"))
